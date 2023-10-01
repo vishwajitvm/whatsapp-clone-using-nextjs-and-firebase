de2190510f9073c9fb51cc9f550ad36bd2c65fad
vishwajitvm <vishwajitmall50@gmail.com>
2023-10-01 14:21:08 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex h-screen overflow-hidden">
+      <h1>Whatsapp cone project</h1>
+    </main>
+  )
+}
